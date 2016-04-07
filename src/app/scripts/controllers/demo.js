ca3eb534c64509520c1040ea61ea1f6b7279a76c
Raymond <ray@firestitch.com>
2016-04-07 18:19:56 -0400

    first commit

---
+++ src/app/scripts/controllers/demo.js
@@ -0,0 +1,22 @@
+'use strict';
+
+
+angular.module('app')
+  .controller('DemoCtrl', function ($scope) {
+
+
+});
+
+
+angular.module('app')
+.directive('fsTagExtend', function() {
+    return {
+        template: '<fs-tag fs-color="tag.color" fs-label="tag.label" fs-tooltip="tag.label"></fs-tag>',
+        restrict: 'E',
+        scope: {
+            tag: '=fsTag'
+        },
+        link: function ($scope, elem, attrs) {
+        } 
+    };
+}); 
---
+++ src/app/scripts/directives/directive.js
@@ -0,0 +1,20 @@
+(function () {
+    'use strict';
+
+    angular.module('fs-angular-tag',[])
+    .directive('fsTag', function() {
+        return {
+            template: '<div class="fs-tag" style="background-color: {{color}};"><md-tooltip ng-show="tooltip">{{tooltip}}</md-tooltip>{{label}}</div>',
+            restrict: 'E',
+            replace: true,
+            scope: {
+                color: '=fsColor',
+                label: '=fsLabel',
+                tooltip: '=fsTooltip'
+            },
+            link: function ($scope, elem, attrs) {
+            } 
+        };
+    });
+     
+})();
